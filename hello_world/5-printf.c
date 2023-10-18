00081df0c1091a96ef437196b61fd03fb0fcede5
KristoffWatral <kristoff.watral@gmail.com>
2023-10-18 00:28:35 -0700

    print

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line using the printf function
- * Retunrs: 0 (Success)
+ * main - this is the function
+ * Retunrs: 0 when successful
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
